993fbee3002ca93c4579ac6b836a170897557cda
Nathan Tucker <ntuckertriplet@gmail.com>
2021-08-24 19:49:27 -0500

    modify resume page

--- pages/resume.js
+++ pages/resume.js
@@ -1,5 +1,16 @@
 import React from "react";
-import { Text, Stack, Divider, Box, Heading, Button, Link, Flex, List, ListItem } from "@chakra-ui/react";
+import {
+  Text,
+  Stack,
+  Divider,
+  Box,
+  Heading,
+  Button,
+  Link,
+  Flex,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
 import { parseISO, format } from "date-fns";
 import Container from "../components/Container";
 
@@ -19,7 +30,7 @@ function Resume({ data }) {
         <Stack isInline>
           <Link
             _hover={{ textDecoration: "none" }}
-            href="https://gist.githubusercontent.com/ntuckertriplet/1dfb7279cf4512603552574f6f20fa27/raw/6f4394e0103b3d3ce38168d733964360461d6dfa/resume.json"
+            href="https://gist.githubusercontent.com/ntuckertriplet/1dfb7279cf4512603552574f6f20fa27/raw/6c72b1db88c37175714a4587263b449a198865f4/resume.json"
             download
           >
             <Button>JSON</Button>
@@ -41,8 +52,12 @@ function Resume({ data }) {
             pt={4}
           >
             <Link href={`tel:${data.basics.phone}`}>{data.basics.phone}</Link>
-            <Link href={`mailto:${data.basics.email}`}>{data.basics.email}</Link>
-            <Link href={`https://maps.google.com/?q=${data.basics.location.city},${data.basics.location.region}`}>
+            <Link href={`mailto:${data.basics.email}`}>
+              {data.basics.email}
+            </Link>
+            <Link
+              href={`https://maps.google.com/?q=${data.basics.location.city},${data.basics.location.region}`}
+            >
               {data.basics.location.city}, {data.basics.location.region}
             </Link>
           </Stack>
@@ -53,9 +68,17 @@ function Resume({ data }) {
             alignSelf="center"
             letterSpacing="tight"
           >
-            <Link href={data.basics.website}>{data.basics.website.replace(/^https?:\/\//, '').replace(/\/$/, "")}</Link>
+            <Link href={data.basics.website}>
+              {data.basics.website
+                .replace(/^https?:\/\//, "")
+                .replace(/\/$/, "")}
+            </Link>
             {data.basics.profiles.map((data) => {
-              return <Link href={data.url}>{`${data.network}: ${data.username}`}</Link>;
+              return (
+                <Link
+                  href={data.url}
+                >{`${data.network}: ${data.username}`}</Link>
+              );
             })}
           </Stack>
           <Divider borderColor="black" />
@@ -71,16 +94,15 @@ function Resume({ data }) {
                 <Heading size="sm">{data.institution}</Heading>
                 <Text>{`${data.studyType}, ${data.area}, ${data.gpa}`}</Text>
               </Box>
-              <Text fontWeight="bold">Intended grad: {format(parseISO(data.endDate), "MMMM, yyyy")}</Text>
+              <Text fontWeight="bold">
+                Intended grad: {format(parseISO(data.endDate), "MMMM, yyyy")}
+              </Text>
             </Flex>
           ))}
           <Divider borderColor="black" />
           <Heading size="lg">Work Experience</Heading>
           {data.work.map((data) => (
-            <Flex
-              flexDirection="column"
-              textAlign="left"
-            >
+            <Flex flexDirection="column" textAlign="left">
               <Flex
                 flexWrap="wrap"
                 flexDirection="row"
@@ -88,8 +110,14 @@ function Resume({ data }) {
                 width="100%"
                 pt={4}
               >
-                <Text><strong>{data.company},</strong> {`${data.position}, ${data.location}`}</Text>
-                <Text fontWeight="bold">{format(parseISO(data.startDate), "MMM")} - {format(parseISO(data.endDate), "MMM, yyyy")}</Text>
+                <Text>
+                  <strong>{data.company},</strong>{" "}
+                  {`${data.position}, ${data.location}`}
+                </Text>
+                <Text fontWeight="bold">
+                  {format(parseISO(data.startDate), "MMM")} -{" "}
+                  {format(parseISO(data.endDate), "MMM, yyyy")}
+                </Text>
               </Flex>
               <List styleType="disc" pr={5}>
                 {data.highlights.map((data) => (
@@ -101,10 +129,7 @@ function Resume({ data }) {
           <Divider borderColor="black" />
           <Heading size="lg">Projects</Heading>
           {data.projects.map((data) => (
-            <Flex
-              flexDirection="column"
-              textAlign="left"
-            >
+            <Flex flexDirection="column" textAlign="left">
               <Flex
                 flexWrap="wrap"
                 flexDirection="row"
@@ -112,7 +137,9 @@ function Resume({ data }) {
                 width="100%"
                 pt={4}
               >
-                <Text><strong>{data.name},</strong> {data.type}</Text>
+                <Text>
+                  <strong>{data.name},</strong> {data.type}
+                </Text>
               </Flex>
               <List styleType="disc" pr={5}>
                 {data.highlights.map((data) => (
@@ -124,10 +151,7 @@ function Resume({ data }) {
           <Divider borderColor="black" />
           <Heading size="lg">Leadership/Activities</Heading>
           {data.leadership.map((data) => (
-            <Flex
-              flexDirection="column"
-              textAlign="left"
-            >
+            <Flex flexDirection="column" textAlign="left">
               <Flex
                 flexWrap="wrap"
                 flexDirection="row"
@@ -135,28 +159,9 @@ function Resume({ data }) {
                 width="100%"
                 pt={4}
               >
-                <Text><strong>{data.organization},</strong> {data.position}</Text>
-              </Flex>
-              <List styleType="disc" pr={5}>
-                {data.highlights.map((data) => (
-                  <ListItem>{data}</ListItem>
-                ))}
-              </List>
-            </Flex>
-          ))}
-          {data.activities.map((data) => (
-            <Flex
-              flexDirection="column"
-              textAlign="left"
-            >
-              <Flex
-                flexWrap="wrap"
-                flexDirection="row"
-                justifyContent="space-between"
-                width="100%"
-                pt={4}
-              >
-                <Text><strong>{data.event},</strong> {data.project}</Text>
+                <Text>
+                  <strong>{data.organization},</strong> {data.position}
+                </Text>
               </Flex>
               <List styleType="disc" pr={5}>
                 {data.highlights.map((data) => (
@@ -168,10 +173,7 @@ function Resume({ data }) {
           <Divider borderColor="black" />
           <Heading size="lg">Awards</Heading>
           {data.awards.map((data) => (
-            <Flex
-              flexDirection="column"
-              textAlign="left"
-            >
+            <Flex flexDirection="column" textAlign="left">
               <Flex
                 flexWrap="wrap"
                 flexDirection="row"
@@ -179,7 +181,10 @@ function Resume({ data }) {
                 width="100%"
                 pt={4}
               >
-                <Text><strong>{data.awarder},</strong> {`${data.title}, ${data.project}`}</Text>
+                <Text>
+                  <strong>{data.awarder},</strong>{" "}
+                  {`${data.title}, ${data.project}`}
+                </Text>
               </Flex>
               <List styleType="disc" pr={5}>
                 {data.highlights.map((data) => (
@@ -189,13 +194,10 @@ function Resume({ data }) {
             </Flex>
           ))}
           <Divider borderColor="black" />
+          <Heading size="lg">Skills</Heading>
           {data.skills.map((data) => (
-            <Stack
-              isInline
-              shouldWrapChildren
-              flexWrap="wrap"
-            >
-              <Heading size="lg">Skills: </Heading>
+            <Stack isInline shouldWrapChildren flexWrap="wrap">
+              <Heading size="md">{data.name}:</Heading>
               {data.keywords.map((data) => (
                 <>
                   <Text>{data}, </Text>
@@ -205,13 +207,13 @@ function Resume({ data }) {
           ))}
         </Flex>
       </Stack>
-    </Container >
+    </Container>
   );
 }
 
 Resume.getInitialProps = async () => {
   const res = await fetch(
-    "https://gist.githubusercontent.com/ntuckertriplet/1dfb7279cf4512603552574f6f20fa27/raw/6f4394e0103b3d3ce38168d733964360461d6dfa/resume.json"
+    "https://gist.githubusercontent.com/ntuckertriplet/1dfb7279cf4512603552574f6f20fa27/raw/6c72b1db88c37175714a4587263b449a198865f4/resume.json"
   );
   const json = await res.json();
   return { data: json };
