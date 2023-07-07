adcce28b9850026d230222f4943f9bec5889646a
njtucker <njtucker@iastate.edu>
2023-07-06 21:06:18 -0500

    i can worry about that later

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,12 +1,10 @@
-import { getPostData } from "@/lib/posts";
 import { useRouter } from "next/router";
 
 const Page = () => {
   const router = useRouter();
   const { slug } = router.query;
-  const postData = getPostData(typeof slug === "string" ? slug : "default");
 
-  return { slug };
+  return <p>{slug}</p>;
 };
 
 export default Page;
--- src/posts/default.md
+++
@@ -1,6 +0,0 @@
----
-title: "First"
-date: "2023-07-08"
----
-
-This is my first post
