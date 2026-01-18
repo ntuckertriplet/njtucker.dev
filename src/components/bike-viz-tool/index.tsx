877ec32011a90c0d6f3732cb3db2c9098133837e
njtucker <ntuckertriplet@gmail.com>
2026-01-18 17:26:53 -0600

    add multiple bikes

--- src/components/bike-viz-tool/index.tsx
+++ src/components/bike-viz-tool/index.tsx
@@ -1,8 +1,7 @@
 "use client";
 
+import { Bike, Geometry } from "@/lib/types";
 import { useEffect, useRef, useState } from "react";
-
-import { Bike } from "@/lib/types";
 
 const BikeVizTool = ({ bikes }: { bikes: Bike[] }) => {
   const [currentManufacturer, setCurrentManufacturer] = useState<string | null>(
@@ -10,6 +9,10 @@ const BikeVizTool = ({ bikes }: { bikes: Bike[] }) => {
   );
   const [currentModel, setCurrentModel] = useState<string | null>(null);
   const [currentSize, setCurrentSize] = useState<number | null>(null);
+
+  const [comparisonBikes, setComparisonBikes] = useState<
+    { size: Geometry; manufacturer: string; model: string }[]
+  >([]);
 
   const PIXEL_SCALAR = 0.25;
 
@@ -20,106 +23,99 @@ const BikeVizTool = ({ bikes }: { bikes: Bike[] }) => {
     if (canvas && context) {
       context.fillStyle = "lightgray";
       context.fillRect(0, 0, canvas.width, canvas.height);
+
+      comparisonBikes.forEach(({ size, manufacturer, model }, index) => {
+        const wheelDiameter = 62.2 / PIXEL_SCALAR;
+        const wheelRadius = wheelDiameter / 2;
+        const commonStartX = wheelRadius + 50;
+        const commonStartY = 600 - wheelRadius;
+
+        context.beginPath();
+        context.arc(commonStartX, commonStartY, wheelRadius, 0, Math.PI * 2);
+        context.stroke();
+
+        context.beginPath();
+        context.arc(
+          commonStartX + size.wheelbase / PIXEL_SCALAR,
+          commonStartY,
+          wheelRadius,
+          0,
+          Math.PI * 2
+        );
+        context.stroke();
+
+        // starting at the common start, we can draw the chainstay
+        const bbDrop = size.bottomBracketDrop / PIXEL_SCALAR;
+        const chainStayLength = size.chainStayLength / PIXEL_SCALAR;
+
+        // Pythagorean theorem to find the bottom bracket position with drop and chainstay length
+        const bbX =
+          commonStartX +
+          Math.sqrt(Math.pow(chainStayLength, 2) - Math.pow(bbDrop, 2));
+        const bbY = commonStartY + bbDrop;
+
+        context.beginPath();
+        context.moveTo(commonStartX, commonStartY);
+        context.lineTo(bbX, bbY);
+        context.stroke();
+
+        // draw the seat tube with BB starting point
+        const seatTubeLength = size.seatTubeLength / PIXEL_SCALAR;
+        const seatTubeAngle = (size.seatTubeAngle * Math.PI) / 180;
+
+        // r * cos(angle), r * sin(angle)
+        const seatX = bbX + seatTubeLength * Math.cos(seatTubeAngle) * -1;
+        const seatY = bbY + seatTubeLength * Math.sin(seatTubeAngle) * -1;
+
+        context.beginPath();
+        context.moveTo(bbX, bbY);
+        context.lineTo(seatX, seatY);
+        context.stroke();
+
+        context.beginPath();
+        context.moveTo(seatX, seatY);
+        context.lineTo(commonStartX, commonStartY);
+        context.stroke();
+
+        // draw the top tube from seat to head tube
+        const handlbarY = bbY - size.stack / PIXEL_SCALAR;
+        const handlebarX = seatX + size.topTubeHorizontal / PIXEL_SCALAR;
+
+        context.beginPath();
+        context.moveTo(seatX, seatY);
+        context.lineTo(handlebarX, handlbarY);
+        context.stroke();
+
+        const headTubeX =
+          handlebarX +
+          (size.headTubeLength / PIXEL_SCALAR) *
+            Math.cos((size.headTubeAngle * Math.PI) / 180);
+        const headTubeY =
+          handlbarY +
+          (size.headTubeLength / PIXEL_SCALAR) *
+            Math.sin((size.headTubeAngle * Math.PI) / 180);
+
+        // draw head tube
+        context.beginPath();
+        context.moveTo(handlebarX, handlbarY);
+        context.lineTo(headTubeX, headTubeY);
+        context.stroke();
+
+        context.beginPath();
+        context.moveTo(headTubeX, headTubeY);
+        context.lineTo(
+          commonStartX + size.wheelbase / PIXEL_SCALAR,
+          commonStartY
+        );
+        context.stroke();
+
+        context.beginPath();
+        context.moveTo(bbX, bbY);
+        context.lineTo(headTubeX, headTubeY);
+        context.stroke();
+      });
     }
-
-    if (currentModel && currentSize && canvas && context) {
-      const bike = bikes.find((b) => b.model === currentModel);
-      if (bike && bike.sizes) {
-        const size = bike.sizes.find((s) => s.frameSize === currentSize);
-        if (size) {
-          // draw 2 wheels of 622mm diameter
-          const wheelDiameter = 62.2 / PIXEL_SCALAR;
-          const wheelRadius = wheelDiameter / 2;
-          const commonStartX = wheelRadius + 50;
-          const commonStartY = 600 - wheelRadius;
-
-          context.beginPath();
-          context.arc(commonStartX, commonStartY, wheelRadius, 0, Math.PI * 2);
-          context.stroke();
-
-          context.beginPath();
-          context.arc(
-            commonStartX + size.wheelbase / PIXEL_SCALAR,
-            commonStartY,
-            wheelRadius,
-            0,
-            Math.PI * 2
-          );
-          context.stroke();
-
-          // starting at the common start, we can draw the chainstay
-          const bbDrop = size.bottomBracketDrop / PIXEL_SCALAR;
-          const chainStayLength = size.chainStayLength / PIXEL_SCALAR;
-
-          // Pythagorean theorem to find the bottom bracket position with drop and chainstay length
-          const bbX =
-            commonStartX +
-            Math.sqrt(Math.pow(chainStayLength, 2) - Math.pow(bbDrop, 2));
-          const bbY = commonStartY + bbDrop;
-
-          context.beginPath();
-          context.moveTo(commonStartX, commonStartY);
-          context.lineTo(bbX, bbY);
-          context.stroke();
-
-          // draw the seat tube with BB starting point
-          const seatTubeLength = size.seatTubeLength / PIXEL_SCALAR;
-          const seatTubeAngle = (size.seatTubeAngle * Math.PI) / 180;
-
-          // r * cos(angle), r * sin(angle)
-          const seatX = bbX + seatTubeLength * Math.cos(seatTubeAngle) * -1;
-          const seatY = bbY + seatTubeLength * Math.sin(seatTubeAngle) * -1;
-
-          context.beginPath();
-          context.moveTo(bbX, bbY);
-          context.lineTo(seatX, seatY);
-          context.stroke();
-
-          context.beginPath();
-          context.moveTo(seatX, seatY);
-          context.lineTo(commonStartX, commonStartY);
-          context.stroke();
-
-          // draw the top tube from seat to head tube
-          const handlbarY = bbY - size.stack / PIXEL_SCALAR;
-          const handlebarX = seatX + size.topTubeHorizontal / PIXEL_SCALAR;
-
-          context.beginPath();
-          context.moveTo(seatX, seatY);
-          context.lineTo(handlebarX, handlbarY);
-          context.stroke();
-
-          const headTubeX =
-            handlebarX +
-            (size.headTubeLength / PIXEL_SCALAR) *
-              Math.cos((size.headTubeAngle * Math.PI) / 180);
-          const headTubeY =
-            handlbarY +
-            (size.headTubeLength / PIXEL_SCALAR) *
-              Math.sin((size.headTubeAngle * Math.PI) / 180);
-
-          // draw head tube
-          context.beginPath();
-          context.moveTo(handlebarX, handlbarY);
-          context.lineTo(headTubeX, headTubeY);
-          context.stroke();
-
-          context.beginPath();
-          context.moveTo(headTubeX, headTubeY);
-          context.lineTo(
-            commonStartX + size.wheelbase / PIXEL_SCALAR,
-            commonStartY
-          );
-          context.stroke();
-
-          context.beginPath();
-          context.moveTo(bbX, bbY);
-          context.lineTo(headTubeX, headTubeY);
-          context.stroke();
-        }
-      }
-    }
-  }, [currentModel, currentSize]);
+  }, [comparisonBikes]);
 
   return (
     <>
@@ -171,6 +167,48 @@ const BikeVizTool = ({ bikes }: { bikes: Bike[] }) => {
             ))
           )}
       </select>
+      <button
+        onClick={() => {
+          setCurrentManufacturer("");
+          setCurrentModel("");
+          setCurrentSize(0);
+
+          const bikeToAdd = bikes.find((bike) => bike.model === currentModel);
+          if (bikeToAdd) {
+            const sizeToAdd = bikeToAdd.sizes.find(
+              (size) => size.frameSize === currentSize
+            );
+            if (sizeToAdd) {
+              setComparisonBikes([
+                ...comparisonBikes,
+                {
+                  size: sizeToAdd,
+                  manufacturer: bikeToAdd.manufacturer,
+                  model: bikeToAdd.model,
+                },
+              ]);
+            }
+          }
+        }}
+      >
+        Add Bike
+      </button>
+      <ul>
+        {comparisonBikes.map((bike, index) => (
+          <li key={index}>
+            {bike.manufacturer} {bike.model} - {bike.size.frameSize}
+            <button
+              onClick={() => {
+                setComparisonBikes(
+                  comparisonBikes.filter((_, i) => i !== index)
+                );
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
